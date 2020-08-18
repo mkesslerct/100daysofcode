3d1f2976e9ba6a6bd3477b635bef3f1d7886fd04
Mathieu Kessler <mathieu.kessler@upct.es>
2020-08-18 16:33:32 +0200

    Completa el day05, movies, usando normalize de unicodedata

--- src/day05_movies.py
+++ src/day05_movies.py
@@ -1,24 +1,27 @@
 import csv
 import collections
+from unicodedata import normalize
 
 def cargar_datos():
-    with open('../data/movie.csv', encoding='utf-8') as fichero:
+    with open('data/movie.csv', encoding='utf-8') as fichero:
         movies_csv = csv.DictReader(fichero)
         movies = collections.defaultdict(list)
         Movie = collections.namedtuple('Movie', 'title year score')
         for line in movies_csv:
             try:
-                movie_title = line['movie_title']
+                movie_title = normalize(
+                    'NFKC',
+                    line['movie_title']).rstrip()
                 director = line['director_name']
             except ValueError:
-                continue
             m = Movie(title = movie_title, year = 2002, score = 3.2)
             movies[director].append(m)
     return(movies)
 
 def main():
     movies_data = cargar_datos()
-    print(type(movies_data))
+    contador = collections.Counter(movies_data)
+    print(contador.most_common(5))
 
  
 if __name__ == "__main__":
